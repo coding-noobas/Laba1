49c2db53c03b759c8cfe2c7b9ecce9696be46e55
coding-noobas <mikolka1979@gmail.com>
2025-09-08 10:49:28 +0300

    Добавьте файлы проекта.

---
+++ Учёба2.1/Учёба2.1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    int n, a[1000000], sum = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        sum += a[i];
+    }
+    cout << sum / n;
+}
---
+++ Учёба2/Учёба2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+    int a = 0;
+    std::cout << "Enter length: ";
+    std::cin >> a;
+    if (a <= 0) {
+        std::cout << "Length must be more than 0, try again: ";
+        std::cin >> a;
+        if (a <= 0) {
+            while (a <= 0) {
+                std::cout << "Bespolezno\n";
+                std::cin >> a;
+            }
+        }
+    }
+    std::cout << "P = " << a * 4 << "\nS = " << a * a << "\n";
+    return 0;
+}
